a140a6b3443335fa7bd85d34c5ecb4e4eeb5fb79
HuilinNi15 <huilin.ni@estudiantat.upc.edu>
2024-10-11 15:16:17 +0200

    Testing SubBytes, ShiftRows, MixColumns

--- pruebas_aes.py
+++ pruebas_aes.py
@@ -13,15 +13,23 @@ algorithm = AES(key=bytearray(16))
 FiniteNumber.set_format('hex')
 
 State = [
-    [0xd4, 0xe0, 0xb8, 0x1e],
-    [0xbf, 0xb4, 0x41, 0x27], 
-    [0x5d, 0x52, 0x11, 0x98],
-    [0x30, 0xae, 0xf1, 0xe5]
+    [0x19, 0xa0, 0x9a, 0xe9],
+    [0x3d, 0xf4, 0xc6, 0xf8], 
+    [0xe3, 0xe2, 0x8d, 0x48],
+    [0xbe, 0x2b, 0x2a, 0x08]
 ]
 
 State = FiniteNumber.matrix_to_FN(State, algorithm.G_F)
 print_FN_matrix(State)
 print()
 
-NewState = algorithm.MixColumns(State)
-print_FN_matrix(NewState)
+SubBytes_state = algorithm.SubBytes(State)
+print_FN_matrix(SubBytes_state)
+print()
+
+ShiftRows_state = algorithm.ShiftRows(SubBytes_state)
+print_FN_matrix(ShiftRows_state)
+print()
+
+MixColumns_state = algorithm.MixColumns(ShiftRows_state)
+print_FN_matrix(MixColumns_state)
